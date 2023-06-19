c6519b54de7d692937a5cf1aa1ccdbfe657e9c26
Mateus B. Salomão <84996618+mateusbsal4@users.noreply.github.com>
2023-06-19 18:33:21 -0300

    Add read_power function

--- src/pzem.cpp
+++ src/pzem.cpp
@@ -7,34 +7,18 @@
  * Pin D1 Rx (Connects to the Tx pin on the PZEM)
  * Pin D2 Tx (Connects to the Rx pin on the PZEM)
 */
-#if defined(ESP32)
 PZEM004Tv30 pzem(Serial2, 16, 17);
-#else
-PZEM004Tv30 pzem(Serial2);
-#endif
 
 
 /************************
- read_voltage
- Retorna leitura da voltagem pelo pzem 
+ read_power
+ Retorna leitura da potência pelo pzem 
  entradas
    nenhuma
  saidas
    nenhuma
 *************************/
-float read_voltage(){
-    return pzem.voltage();
+float read_power(){
+    return pzem.power();
 }
 
-
-/************************
- read_current
- Retorna leitura da corrente pelo pzem
- entradas
-   nenhuma
- saidas
-   nenhuma
-*************************/
-float read_current(){
-    return pzem.current();
-}
