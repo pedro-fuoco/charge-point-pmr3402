692e4b2edc6e4fbdd5e0bb9c885410d090f6a965
Mateus B. Salomão <84996618+mateusbsal4@users.noreply.github.com>
2023-06-19 19:30:58 -0300

    Fix lex strip activation

--- src/leds.cpp
+++ src/leds.cpp
@@ -7,6 +7,11 @@ int pinR;
 int pinG;
 int pinB;
 
+// setting PWM properties
+const int freq = 5000;
+const int ledChannel = 0;
+const int resolution = 8;
+
 
 
 /************************
@@ -18,12 +23,15 @@ int pinB;
    nenhuma
 *************************/
 void leds_setup(int pin1, int pin2, int pin3){
+    // configure LED PWM functionalitites
+    ledcSetup(ledChannel, freq, resolution);
     pinR = pin1;
-    pinG = pin1;
-    pinB = pin1;
-    pinMode(pinR, OUTPUT);
-    pinMode(pinB, OUTPUT);
-    pinMode(pinG, OUTPUT);    
+    pinG = pin2;
+    pinB = pin3;
+  // attach the channel to the GPIO to be controlled
+    ledcAttachPin(pinR, ledChannel);
+    ledcAttachPin(pinG, ledChannel);
+    ledcAttachPin(pinB, ledChannel);
 }
 
 /************************
@@ -38,10 +46,15 @@ void activate_leds(int n){ // n varia de 1 a 7
     int bit0 = (n & 0b001); 
     int bit1 = (n & 0b010) >> 1;
     int bit2 = (n & 0b100) >> 2;
-    digitalWrite(pinR, bit0);
-    digitalWrite(pinG, bit1);
-    digitalWrite(pinB, bit2);    
+    int dc0 = (bit0) ? 255:0;    
+    int dc1 = (bit1) ? 255:0;
+    int dc2 = (bit2) ? 255:0;    
+    ledcWrite(pinR, dc0);
+    ledcWrite(pinG, dc1);
+    ledcWrite(pinB, dc2);    
 }
+
+
 
 
 
